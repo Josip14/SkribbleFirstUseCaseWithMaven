1c17e32d1775d5cecfe879348a08e15355a011a9
JosipHarambasic <harambasic.josip97@gmail.com>
2021-07-26 14:10:20 +0200

    clean up code

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,22 +9,22 @@ import org.apache.pdfbox.pdmodel.interactive.form.*;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, CsvException {
+    public static void main(@org.jetbrains.annotations.NotNull String[] args) throws IOException, CsvException {
 
         // example of the .csv file path args[0]-> "C:\\Users\\41786\\OneDrive\\Desktop\\Data.csv";
         List<String[]> persons;
         try (CSVReader reader = new CSVReader(new FileReader(args[0]))) {
-            List<String[]> readAllPers = reader.readAll();
-            persons = readAllPers;
-            readAllPers.forEach(x -> System.out.println(Arrays.toString(x)));
+            persons = reader.readAll();
+            /**
+             * See what the the reader outputs
+             * persons.forEach(x -> System.out.println(Arrays.toString(x)));
+             */
         }
-
         // example of a pdf file path with formFields args[1]-> "C:\\Users\\41786\\OneDrive\\Desktop\\FormField.pdf";
         // need to set the arguments
         PDDocument pdfDocument = Loader.loadPDF(new File(args[1]));
@@ -33,9 +33,9 @@ public class Main {
         List<PDField> fields = acroForm1.getFields();
 
         // here we print the names of the fields out so we see what the names are from it
-        for(int i = 0; i<fields.size(); i++){
+        /**for(int i = 0; i<fields.size(); i++){
          System.out.println(fields.get(i).getPartialName());
-        }
+        }*/
 
         for (int i = 1; i<persons.size(); i++){
             try (PDDocument pdfDocument1 = Loader.loadPDF(new File(args[1]))) {
@@ -43,17 +43,15 @@ public class Main {
                 PDAcroForm acroForm = pdfDocument1.getDocumentCatalog().getAcroForm();
                 // as there might not be an acroForm entry a null check is necessary
                 if (acroForm != null) {
-                    // Retrieve an individual field and set its value.
-                    // We can add as much as there are form field to fill them out.
-                    // we can retrieve the names of the form fields with the code above with the forLoop
+                    //We get the fields and set their values to the values of the .csv file
                     for (int j = 0; j < fields.size(); j++) {
                         acroForm.getField(fields.get(j).getPartialName()).setValue(persons.get(i)[j]);
+                    }
+                    // Save the filled out PDF
+                    //savingPath example args[2] ->  C:\\Users\\41786\\OneDrive\\Desktop
+                    pdfDocument1.save(args[2] + "\\"+ persons.get(i)[0] + ".pdf");
                 }
-            // Save and close the filled out form.
-            //savingPath example args[2] ->  C:\\Users\\41786\\OneDrive\\Desktop
-            pdfDocument1.save(args[2] + "\\"+ persons.get(i)[0] + ".pdf");
             }
         }
     }
-}
 }
