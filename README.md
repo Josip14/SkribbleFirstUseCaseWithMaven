7bd1d72a4ed43511762722771560481229a00fb5
JosipHarambasic <harambasic.josip97@gmail.com>
2021-07-27 08:55:45 +0200

    added double name prevention

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ## Skribble programmatically filling out PDF's with form fields using a .csv file
 What are we doing here?
-- we make an application that takes 3 arguments(csvFilePath, pdfFilePath, savingLocation)
+- we make an application that takes 3 arguments (csvFilePath, pdfFilePath, savingLocation)
 - the .csv file has to have the same order as the PDF form fields
-  for this you can easily comment the "for loop" out, and it prints you in the console the right order
+  for this you can easily comment the "for loop" out, and it prints you in the console the right order, and the correct type
 - finally, every pdf which is filled out gets saved on the local machine in the path you specified
 
-You can have a look at these webpages to get more information about the hole project,
-there are information I looked at, so the application works properly
+You can have a look at these webpages to get more information, if you want
+to extend this code or to go into more details
 - https://mkyong.com/java/how-to-read-and-parse-csv-file-in-java/
 - https://svn.apache.org/viewvc/pdfbox/trunk/examples/src/main/java/org/apache/pdfbox/examples/interactive/form/
 - https://svn.apache.org/viewvc/pdfbox/trunk/examples/src/main/java/org/apache/pdfbox/examples/interactive/form/FillFormField.java?revision=1873147&view=markup  
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -20,18 +20,14 @@ public class Main {
         List<String[]> persons;
         try (CSVReader reader = new CSVReader(new FileReader(args[0]))) {
             persons = reader.readAll();
-            /**
-             * See what the reader outputs
-             * persons.forEach(x -> System.out.println(Arrays.toString(x)));
-             */
         }
 
-        // here we print the order of the formFields so we know how the .csv file should look like
         /**
-         for(int i = 0; i<fields.size(); i++){
-         System.out.println(fields.get(i).getPartialName());
-        }*/
+         * See what the reader outputs
+         * persons.forEach(x -> System.out.println(Arrays.toString(x)));
+         */
 
+        // Start iteration from second row because the first is the header
         for (int i = 1; i<persons.size(); i++){
             try {
                 // example of a pdf file path with formFields args[1]-> "C:\\Users\\41786\\OneDrive\\Desktop\\FormField.pdf";
@@ -41,23 +37,39 @@ public class Main {
                 PDAcroForm acroForm = documentCatalog.getAcroForm();
                 List<PDField> fields = acroForm.getFields();
 
+                /**
+                 * If you want to check what the order and what type the fields have uncomment the code below
+                for (int l = 0; l<fields.size();l++){
+                    System.out.println(fields.get(l).getPartialName());
+                    System.out.println(fields.get(l).getFieldType());
+                }
+                 */
+
                 // Check for the right length of the rows so it gets filled correctly else we see which row is not complete
                 if (persons.get(i).length < persons.get(0).length) {
                     System.out.println("Row number " + i + " is not complete and can cause false filling of the form fields in the PDF: "
                             + (persons.get(0).length - persons.get(i).length) + " argument(s) is/are missing");
                 }
+
                 else if (persons.get(i).length > persons.get(0).length){
                     System.out.println("Row number " + i + " has too much arguments and causes an error in the PDF: "
                             + (persons.get(i).length - persons.get(0).length) + " argument(s) is/are redundant" );
                 }
-                // set the values of the .csv file into the form fields
+
+                /**
+                 * this line makes everything happen and writes the data from the .csv file into
+                 * the empty PDF for each row in the .csv file
+                 */
                 for (int j = 0; j < persons.get(i).length; j++) {
                     acroForm.getField(fields.get(j).getPartialName()).setValue(persons.get(i)[j]);
                 }
 
-                // Save the filled out PDF
-                // savingPath example args[2] ->  C:\\Users\\41786\\OneDrive\\Desktop
-                pdfDocument.save(args[2] + "\\"+ persons.get(i)[0] + ".pdf");
+                /**
+                 * Save the filled out PDF
+                 * savingPath example args[2] ->  C:\\Users\\41786\\OneDrive\\Desktop
+                 * the i is just to prevent errors if there are more the one of the same name, but you can change it as you like
+                 */
+                pdfDocument.save(args[2] + "\\" + persons.get(i)[0] + " " + i + ".pdf");
 
             } catch (IOException e) {
                 e.printStackTrace();
